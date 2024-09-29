1b4326bd2cf5302f00b7982e2b22406b53f68f9d
Roman Farooq <theromanfarooq@gmail.com>
2024-09-29 21:45:13 +0500

    added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
-      Tradex React App Comming Soon!
-    </h1>
-  )
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">Tradex React App Comming Soon!</h1>
+    </div>
+  );
 }

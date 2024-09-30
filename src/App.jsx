f08488b9e78feffbb147887ed28bd44ad059298c
Shahidch19 <chshahidofficial119@gmail.com>
2024-09-30 23:18:50 +0500

    some routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Header from "./components/Header";
+import AdminLayout from "./layouts/AdminLayout";
+import Dashboard from "./components/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "header",
+        element: <Header />,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    element: <AdminLayout />,
+    children: [
+      {
+        path: "dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <h1 className="text-3xl font-bold">Tradex React App Comming Soon!</h1>
-    </div>
+   <RouterProvider router={router} />
   );
 }
